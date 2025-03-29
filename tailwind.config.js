e5ad630abd5fb79bd0e1f0a89018b1ff647f3343
Guilherme-lgs <melloguilerme38@gmail.com>
2025-03-29 12:50:44 -0300

    teste subida arquivos

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ['./src/**/*.{html,js}'], // Inclui todos os arquivos HTML e JS no diretório src
+  theme: {
+    extend: {
+      fontFamily: {
+        lilita: ['Lilita One', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+};
